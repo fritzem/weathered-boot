31e3d45df18a06b9ea93b2d82cd8d5650d12d830
fritzem <50849645+fritzem@users.noreply.github.com>
2021-10-28 22:20:42 -0400

    Query multiple sensors with a json array

--- src/main/java/com/fritzem/weatherstation/controller/SensorController.java
+++ src/main/java/com/fritzem/weatherstation/controller/SensorController.java
@@ -1,12 +1,16 @@
 package com.fritzem.weatherstation.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fritzem.weatherstation.model.Report;
 import com.fritzem.weatherstation.repository.ReportRepository;
 import com.fritzem.weatherstation.repository.SensorRepository;
 import com.fritzem.weatherstation.model.Sensor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class SensorController {
@@ -19,22 +23,27 @@ public class SensorController {
         this.reportRepository = reportRepository;
     }
 
+    /*
+     * Generic message for base url. Maybe make this a help function.
+     */
     @RequestMapping
     public String about() {
         return "This is the weather station. Beep boop.";
     }
 
-    /**
-    @GetMapping("/temp")
-    public Sensor sensor(@RequestParam(value = "temp", defaultValue = "") String temp) {
-        return new Sensor(Integer.parseInt(temp));
-    } */
-
+    /*
+     * Returns a sensor based on id
+     */
     @GetMapping("/{id}")
     public Sensor checkSensor(@PathVariable String id) {
         return sensorRepository.findById(Long.parseLong(id));
     }
 
+    /*
+     * Creates a sensor
+     * @param country
+     * @param city
+     */
     @PostMapping("/create") //RequestBody for raw json, ModelAttribute for form data
     public Sensor create(@RequestBody Map<String, String> body) {
         Sensor sensor = new Sensor(
@@ -45,6 +54,11 @@ public class SensorController {
         return sensor;
     }
 
+    /*
+     * Generates a report for a sensor
+     * @param temperature
+     * @param humidity
+     */
     @PostMapping("/{id}/report")
     public Report report(
             @PathVariable String id,
@@ -56,5 +70,35 @@ public class SensorController {
         );
         reportRepository.save(report);
         return report;
+    }
+
+    /*
+     * Queries reports
+     * @param sensorIds ~ list of sensor ids to include, leave blank to query all sensors
+     */
+    @PostMapping("/reports")
+    public List<Sensor> report(
+            @RequestBody JsonNode payload) {
+
+        List<Sensor> sensors;
+
+        ArrayList<Long> sensorIds = new ArrayList<>();
+
+        JsonNode payloadIds = payload.get("sensorIds");
+        if (payloadIds != null && payloadIds.isArray() && !payloadIds.isEmpty()) {
+            for (JsonNode jn : payloadIds) {
+                sensorIds.add(Long.parseLong(jn.toString()));
+            }
+            sensors = sensorRepository.findAllById(sensorIds);
+        } else {
+            sensors = sensorRepository.findAll();
+        }
+
+        return sensors;
+
+
+
+
+
     }
 }
--- src/main/java/com/fritzem/weatherstation/repository/ReportRepository.java
+++ src/main/java/com/fritzem/weatherstation/repository/ReportRepository.java
@@ -1,8 +1,11 @@
 package com.fritzem.weatherstation.repository;
 
 import com.fritzem.weatherstation.model.Report;
+import com.fritzem.weatherstation.model.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    Report findBySensor(Sensor s);
 
 }
